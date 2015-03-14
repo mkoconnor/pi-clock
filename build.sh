12cf75b4c951c56d845f509b1df1ce52641097c2
Michael O'Connor <mkoconnor@gmail.com>
2015-03-14 19:23:29 -0400

    new approach

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ocamlfind ocamlc -package js_of_ocaml -package js_of_ocaml.syntax -syntax camlp4o -linkpkg -o pi_clock.byte -rectypes -custom pi_clock.ml -verbose && js_of_ocaml pi_clock.byte

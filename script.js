62a845d53ac3302a15a957019023d01d96eb5176
Michael O'Connor <mkoconnor@gmail.com>
2015-03-14 19:23:47 -0400

    add script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+var nowInMilliseconds = function () {
+    var now = new Date(Date.now ());
+    var acc = 0;
+    acc += now.getHours ();
+    acc *= 60;
+    acc += now.getMinutes ();
+    acc *= 60;
+    acc += now.getSeconds ();
+    acc *= 1000;
+    acc += now.getMilliseconds ();
+    return acc;
+}
+
+$(document).ready(function () {
+    var current_index = 0;
+    var advance_index = function (now) {
+        if (now > 86340840) // the maximum number of milliseconds in pi_powers
+        {
+            current_index = 0;
+            return;
+        }
+        while (pi_powers[current_index].milliseconds < now) {
+            current_index += 1;
+        }
+    };
+        
+    setInterval(function () {
+        var now = nowInMilliseconds();
+        advance_index (now);
+        $("#milliseconds").html(now);
+        $("#pipower").html(pi_powers[current_index].pi_power);
+    }, 100);
+});
